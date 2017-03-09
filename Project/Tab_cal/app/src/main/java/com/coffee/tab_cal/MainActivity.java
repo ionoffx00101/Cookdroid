2c39cf2021f0f5277270f11aac4b444116366f46
teQoo <anes2354@gmail.com>
2017-03-09 21:05:55 +0900

    17.03.09_02
    탭타이탄 계산기 모작

--- Project/Tab_cal/app/src/main/java/com/coffee/tab_cal/MainActivity.java
+++ Project/Tab_cal/app/src/main/java/com/coffee/tab_cal/MainActivity.java
@@ -1,20 +1,25 @@
 package com.coffee.tab_cal;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.japisoft.formula.Formula;
+import com.japisoft.formula.FormulaFactory;
+import com.japisoft.formula.Variant;
+import com.japisoft.formula.node.EvaluateException;
 
 public class MainActivity extends AppCompatActivity {
+  FormulaFactory f;
 
   // 계산중인걸 저장하는 공간을 만들어둔다.
-  String saveTextInjava="";
+  String saveTextInjava = "";
+  String littlesaveTextInjava = "";
   String resultTextInjava;
 
-  Double resultDoubleSemi=0.0;
-  Double resultDouble=0.0;
+  Double resultDouble = 0.0;
   // 리턴 받을 공간 만들어두기
   TextView saveTextView;
   TextView resultTextView;
@@ -25,61 +30,53 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     // 객체 리턴 받는다.
-    saveTextView =(TextView)findViewById(R.id.save_text);
-    resultTextView =(TextView)findViewById(R.id.result_text);
+    saveTextView = (TextView) findViewById(R.id.save_text);
+    resultTextView = (TextView) findViewById(R.id.result_text);
 
 
   }
-  public void btnClick(View view){
+
+  public void btnClick(View view) {
     // v.getId() 버튼 id
     // v.getText() 버튼 text
 
     int clickId = view.getId();
-    String clickText = ((Button)view).getText().toString();
+    String clickText = ((Button) view).getText().toString();
 
     //Toast.makeText(MainActivity.this,clickText+"",Toast.LENGTH_SHORT).show();
 
-    if(clickText.equals("+") || clickText == "+"){
+    if (clickText.equals("=") || clickText == "=") {
 
-    }else if(clickText.equals("-") || clickText == "-"){
+      Formula f = new Formula(saveTextInjava);
+      Variant variant = null;
 
-    }else if(clickText.equals("*") || clickText == "*"){
-
-    }else if(clickText.equals("/") || clickText == "/"){
-
-    }else if(clickText.equals("C") || clickText == "C"){
-
-    }else if(clickText.equals("ㅁ") || clickText == "ㅁ"){
-
-    }
-//    else {
-//
-//    }
-
-
-
-
-
-    if(clickText.equals("=") || clickText == "="){
-      Toast.makeText(MainActivity.this,clickText+"",Toast.LENGTH_SHORT).show();
-      //계산 그냥 그때그때 받아서 처리하는게 ㅇㄷ인듯 ㅇㅇ ㄱ
-//Fomula 공부하고 마저 하자..
-
-
-      resultTextInjava = clickText + resultDouble;
-      resultTextView.setText(resultTextInjava);
-    }else {
-
-//      if(!saveTextInjava.substring(saveTextInjava.length()).equals(".") && saveTextInjava.substring(saveTextInjava.length()) != ".") {
-//        saveTextInjava += " ";
-//      }
-
-      if(!clickText.equals(".") && clickText != ".") {
-        saveTextInjava += " ";
+      try {
+        variant = f.evaluate();
+      } catch (EvaluateException e) {
+        e.printStackTrace();
       }
+      saveTextView.setText(saveTextInjava);
+      resultTextView.setText("=" + variant.toString());
+      // 개체 값 초기화
+      saveTextInjava = "";
+      resultTextInjava = "";
+      littlesaveTextInjava = "";
+    } else if (clickText.equals("C") || clickText == "C") {
+      saveTextInjava = "0";
+      resultTextInjava = "0";
+      saveTextView.setText(saveTextInjava);
+      resultTextView.setText(resultTextInjava);
+    } else if (clickText.equals("ㅁ") || clickText == "ㅁ") {
+      MainActivity.this.finish();
+    } else if ((clickText.equals("+") || clickText == "+") || (clickText.equals("-") || clickText == "-") || (clickText.equals("*") || clickText == "*") || (clickText.equals("/") || clickText == "/")) {
       saveTextInjava += clickText;
       saveTextView.setText(saveTextInjava);
+      resultTextView.setText("0");
+      littlesaveTextInjava = "";
+    } else {
+      saveTextInjava += clickText;
+      littlesaveTextInjava += clickText;
+      resultTextView.setText(littlesaveTextInjava);
     }
-
   }
 }
